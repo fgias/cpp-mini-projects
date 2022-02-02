a92e6de9f7555695d1c274b6346ff0bff8379df0
fgias <fotisfigz@gmail.com>
2022-02-02 18:26:38 +0200

    Outputting text

---
+++ Outputting text/outputting_text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Starting program..." << flush;
+    cout << "Done." << endl;
+    cout << "This is some text.";
+    cout << " On the same line." << endl;
+    cout << "Banana. " << "Apple. " << "Orange " << endl;
+    cout << "This is some more text." << endl;
+    return 0;
+}
