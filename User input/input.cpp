acb12e0ce7da98251ad32a8ecc81d95f5b5bcfea
fgias <fotisfigz@gmail.com>
2022-02-04 15:59:56 +0200

    User input

---
+++ User input/input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Enter your name: " << flush;
+
+    string input; 
+
+    cin >> input; 
+
+    cout << "Your name is: " << input << endl;
+
+    return 0;
+}
