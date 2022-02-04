82e245d94f59995cba53ec7d54f9cc437f9e0d0a
fgias <fotisfigz@gmail.com>
2022-02-04 20:02:09 +0200

    While

---
+++ C++ for Complete Beginners/While/while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i = 0;
+
+    while (i <= 5) {
+        cout << i << endl;
+        
+        i++; // or i += 1, or i = i + 1
+    }
+
+    cout << "Program quitting..." << endl;
+    return 0;
+}
