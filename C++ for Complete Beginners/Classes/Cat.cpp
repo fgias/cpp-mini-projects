d604a2055af8af74249431ae5d9a2bf471a9ec3a
Fotios Ioannis Giasemis <fotisfigz@gmail.com>
2022-02-10 02:37:09 +0200

    Classes

---
+++ C++ for Complete Beginners/Classes/Cat.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Cat.h" // not necessary
+
+using namespace std;
+
+void speak() {
+    cout << "Meowww!" << endl;
+}
---
+++ C++ for Complete Beginners/Classes/classes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Cat.h"
+using namespace std;
+
+// you have to compile the two files TOGETHER
+// g++ classes.cpp Cat.cpp -o classes
+
+int main() {
+    speak();
+    return 0;
+}
