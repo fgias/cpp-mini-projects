0b372d2406cd54ba0ace26d8d8290cf74ff59dd6
Fotios Ioannis Giasemis <fotisfigz@gmail.com>
2022-02-10 03:00:38 +0200

    Defining classes in .h files or in main executable

--- C++ for Complete Beginners/Classes/classes.cpp
+++ C++ for Complete Beginners/Classes/classes.cpp
@@ -5,12 +5,30 @@ using namespace std;
 // you have to compile the two files TOGETHER
 // g++ classes.cpp Cat.cpp -o classes
 
+// you can also define a class here
+class Dog {
+    public:
+    void run() {
+        cout << "Dog is running." << endl;
+    };
+    void swim();
+};
+
+void Dog::swim() {
+    cout << "Dog is swimming." << endl;
+};
+
 int main() {
 
     Cat cat; // same as `int value;` etc
 
     cat.speak();
     cat.jump();
+
+    Dog dog;
+
+    dog.run();
+    dog.swim();
 
     return 0;
 }
