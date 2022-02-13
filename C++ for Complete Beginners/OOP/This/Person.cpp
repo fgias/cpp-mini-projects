954540bb5dcdcf242d2c169fceac64bfacce7473
Fotios Ioannis Giasemis <fotisfigz@gmail.com>
2022-02-13 22:54:21 +0200

    This pointer

--- C++ for Complete Beginners/OOP/This/Person.cpp
+++ C++ for Complete Beginners/OOP/This/Person.cpp
@@ -11,10 +11,16 @@ Person::Person() {
 Person::Person(string name, int age) {
     this->name = name; // without "this", name assumes the most closely defined variable
     this->age = age;
+    cout << "Memory address of the object: " << this << endl;
+
+    // Every object in C++ has access to its own address through an important pointer called `this` pointer. 
+    // The this pointer is an implicit parameter to all member functions. Therefore, inside a member function, 
+    // this may be used to refer to the invoking object.
 };
 
 string Person::toString() {
     stringstream ss;
-    ss << "The person's name is " << name << " and he is " << age << " years old.";
+    ss << " -- The person's name is " << name << " and he is " << age << " years old.";
+    ss << " Memory location: " << this;
     return ss.str();
 }
--- C++ for Complete Beginners/OOP/This/this.cpp
+++ C++ for Complete Beginners/OOP/This/this.cpp
@@ -9,7 +9,10 @@ int main() {
     cout << person1.toString() << endl;
 
     Person person2("Bob", 32);
-    cout << person2.toString() << endl;
+    cout << person2.toString() << endl << "Also with `&obj`: " << &person2 << endl;
+
+    Person person3("Sam", 72);
+    cout << person3.toString() << endl << "Also with `&obj`: " << &person3 << endl;
 
     return 0;
 }
