f31ac26a4c4df09c5c392f20676a26b47aa8d2cb
Fotios Ioannis Giasemis <fotisfigz@gmail.com>
2022-02-14 13:52:35 +0200

    Pointers

--- C++ for Complete Beginners/4. Pointers and Memory/pointers.cpp
+++ C++ for Complete Beginners/4. Pointers and Memory/pointers.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 using namespace std;
 
+void manipulate(double value) {
+    cout << "2. Value of double in manipulate:" << value << endl;
+    value = 10;
+    cout << "3. Value of double in manipulate: " << value << endl;
+};
+
+
+
 int main() {
 
     int nValue = 8;
 
+    int* pnValue = &nValue; // `int*` = "int pointer", which means a pointer to an int, 
+    // `&nValue` = "memory address of nValue"
+
     nValue = 9;
 
     cout << "int value: " << nValue << endl;
+    cout << "pointer to int address: " << pnValue << endl; // memory address is a number in hexadecimal (0x prefix)
+
+    cout << "int value via pointer: " << *pnValue << endl; // this is called "dereferencing a pointer"
+
+    cout << endl << "=================================" << endl;
+    double dValue = 123.4;
+    cout << "1. dValue: " << dValue << endl;
+    manipulate(dValue);
+    cout << "4. dValue: " << dValue << endl;
+
+
 
     return 0;
-}
+};
+
+
+// int*: type of the variable, pointer to an int
+// &: get memory address
+// *pnValue: access the value that the pointer points to
