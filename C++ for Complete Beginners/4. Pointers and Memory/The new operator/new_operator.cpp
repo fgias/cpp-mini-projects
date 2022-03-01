9f7914db77621b842b71bbe67e376069990f6631
Fotios Ioannis Giasemis <fotisfigz@gmail.com>
2022-03-01 18:04:34 +0200

    The new operator, returning a pointer to an object

--- C++ for Complete Beginners/4. Pointers and Memory/The new operator/new_operator.cpp
+++ C++ for Complete Beginners/4. Pointers and Memory/The new operator/new_operator.cpp
@@ -16,13 +16,27 @@ public:
         cout << "Animal created by copying." << endl;
     };
 
-    ~Animal() {cout << "Destructor called." << endl;}; // destructor
+    ~Animal() {
+        cout << "Destructor called." << endl;
+    }; // destructor
 
-    void setName(string name) {this->name = name;};
+    void setName(string name) {
+        this->name = name;
+    };
 
-    void speak() const {cout << "My name is " << name << "." << endl;};
+    void speak() const {
+        cout << "My name is " << name << "." << endl;
+    };
 
 };
+
+Animal* createAnimal() {
+    Animal* pAnimal = new Animal(); // the new operator here avoids the object being destryoed
+    // when reaching the outer bracket `}`
+    // the memory will not be deallocated
+    pAnimal->setName("Bertie");
+    return pAnimal;
+}
 
 int main() {
 
@@ -31,19 +45,28 @@ int main() {
     // cat.speak();
 
     Animal *pCat = new Animal(); // the new operator allocates memory explicitly for a single object
-    (*pCat).setName("Sammy");
-    (*pCat).speak();
-
-    // you can do this more easily with `pCat->setName("Sammy")`
-    // instead of `(*pCat).setName("Sammy")`
-    pCat->setName("Bobby");
+    pCat->setName("Sammy");
     pCat->speak();
 
     delete pCat; // when using new, we have to manually deallocate the memory with delete
     // i.e. we have to call the destructor manually
     // always call DELETE with NEW
 
-    cout << sizeof(pCat) << endl;
+    Animal* pFrog = createAnimal(); // `Animal*`: a pointer to an animal
+    // more efficient to return a reference to an object, than the object itself
+    pFrog->speak();
+    delete pFrog;
 
     return 0;
 }
+
+// Memory: Stack vs Heap
+// - Stack: 
+//  * local variables 
+//  * for example in main() `int value = 3`
+//  * function calls
+//  * exceeding the amount of memory: "stack overflow"
+// - Heap: 
+//  * all the memory available to the computer
+//  * for example with operator `new`
+//  * a large object needs to be allocated it to the heap, in order to avoid stack overflow
