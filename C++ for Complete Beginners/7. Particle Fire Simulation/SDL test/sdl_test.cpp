5c29a2d25b4391118ad77cffce96ef4173654e27
Fotios Ioannis Giasemis <fotisfigz@gmail.com>
2022-03-16 19:42:53 +0200

    SDL successfully initialized

--- C++ for Complete Beginners/7. Particle Fire Simulation/SDL test/sdl_test.cpp
+++ C++ for Complete Beginners/7. Particle Fire Simulation/SDL test/sdl_test.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cout << "SDL init failed." << endl;
+        return 1;
+    }
+
+    cout << "SDL init succeeded." << endl;
+
+    SDL_Quit();    
+
     return 0;
 }
