fe2fb2e8c2ad9d2aca9a40d923a3c7e4ff21d2aa
Fotios Ioannis Giasemis <fotisfigz@gmail.com>
2022-04-07 21:56:35 +0300

    Template Functions and Type Inference

--- Advanced C++/5. Template Classes and Functions/Template functions/template_functions.cpp
+++ Advanced C++/5. Template Classes and Functions/Template functions/template_functions.cpp
@@ -5,14 +5,28 @@ using namespace fgias;
 
 template<class T> // you can also use `typename` instead of `class`
 void print(T obj) {
-    cout << obj << endl;
+    cout << "Template version: " << obj << endl;
+}
+
+void print(int value) {
+    cout << "Non-template version: " << value << endl;
+}
+
+template<class T>
+void show() {
+    cout << T() << endl;
 }
 
 int main() {
 
     print<int>(5);
     print<string>("hello");
-    print<Complex>(Complex(2.1, 3.2));
+    print<>(Complex(2.1, 3.2));
 
+    print(6);
+    print<>(6);
+
+    show<int>();
+    
     return 0;
 }
