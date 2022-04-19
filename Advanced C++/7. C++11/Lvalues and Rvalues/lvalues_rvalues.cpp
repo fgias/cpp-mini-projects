d47c6b019f19dafa5269e395bcba4fa653c957f8
Fotios Ioannis Giasemis <fotisfigz@gmail.com>
2022-04-19 11:26:04 +0300

    Lvalues and Rvalues

--- Advanced C++/7. C++11/Lvalues and Rvalues/lvalues_rvalues.cpp
+++ Advanced C++/7. C++11/Lvalues and Rvalues/lvalues_rvalues.cpp
@@ -9,11 +9,11 @@ private:
     int* _pBuffer;
 public:
     Test() {
-        cout << "constructor" << endl;
+        // cout << "constructor" << endl;
         _pBuffer = new int[SIZE]{}; // allocate memory and set to zero
     }
     Test(int i) {
-        cout << "parameterized constructor" << endl;
+        // cout << "parameterized constructor" << endl;
         _pBuffer = new int[SIZE]{};
 
         for (int i=0; i<SIZE; i++) {
@@ -21,26 +21,26 @@ public:
         }
     }
     Test(const Test& other) {
-        cout << "copy constructor" << endl;
+        // cout << "copy constructor" << endl;
         _pBuffer = new int[SIZE]{};
         memcpy(_pBuffer, other._pBuffer, sizeof(int)*SIZE);
         // _pBuffer[i] = other._pBuffer[i]
     }
     Test& operator=(const Test& other) {
-        cout << "assignment" << endl; 
+        // cout << "assignment" << endl; 
         _pBuffer = new int[SIZE]{};
         memcpy(_pBuffer, other._pBuffer, sizeof(int)*SIZE);
         return *this;
     }
     ~Test() {
-        cout << "destructor" << endl;
+        // cout << "destructor" << endl;
         delete [] _pBuffer;
     }
     friend ostream& operator<<(ostream& out, const Test& test);
 };
 
 ostream& operator<<(ostream& out, const Test& test) {
-    cout << "hello from test" << endl;
+    // cout << "hello from test" << endl;
     return out;
 }
 
@@ -51,19 +51,35 @@ Test getTest() {
 int main() {
 
     Test test1 = getTest();
-    cout << test1 << endl;
 
     vector<Test> vec;
     vec.push_back(Test());
 
+    int value1 = 7;
+
+    int* pValue1 = &value1; // lvalue
+    // int* pValue2 = &7; // rvalue - does not work
+
+    Test* pTest1 = &test1; // lvalue
+    // Test* pTest2 = &getTest(); // rvalue – does not work because it is has a temporary value
+
+    int* pValue3 = &++value1; // ++value1: lvalue, works with pre-fix operator
+    // int* pValue4 = &value1++; // value1++: rvalue, does not work with the post-fix operator
+    cout << *pValue3 << endl;
+
+    // int* pValue5 = &(7 + value1);
+
     return 0;
 }
 
-
 /*
 
-to disable copy elision use the flag -fno-elide-constructors:
+In general, lvalue = left, rvalue = right in an assingment:
+lvalue = rvalue.
 
-g++ -std=c++11 elision.cpp -o elision -fno-elide-constructors; ./elision
+In c++, this idea is extended. 
+Lvalue: anything you can take the address of
+Rvalue: anything you cannot take the address of
+
 
 */
