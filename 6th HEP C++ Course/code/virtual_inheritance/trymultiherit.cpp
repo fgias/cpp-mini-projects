d2c134ae4a91871ae31487aa8d8533387778b3e0
Fotis Giasemis <Fotis.Giasemis@cern.ch>
2023-03-08 16:16:46 +0100

    Virtual inheritance

--- 6th HEP C++ Course/code/virtual_inheritance/trymultiherit.cpp
+++ 6th HEP C++ Course/code/virtual_inheritance/trymultiherit.cpp
@@ -3,10 +3,13 @@
 
 int main() {
     // create a TextBox and call draw
-
+    TextBox textbox("Hello", 20, 50);
+    Rectangle rectangle(12345, 40, 10);
 
     // Fix the code to call both draws by using types
-
+    rectangle.draw();
+    Rectangle& ref = textbox;
+    ref.draw();
 
     // try with virtual inheritance
 
